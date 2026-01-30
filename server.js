8e98daebfdacb8402ffe2d6cc44ea33ef03e3ae8
CsCheems <j.dev.mx@outlook.com>
2026-01-30 01:06:42 -0600

    primer commit

---
+++ server.js
@@ -0,0 +1,182 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import fetch from "node-fetch";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+const STEAM_KEY = "";
+const STEAM_ID = "";
+
+let cache = {
+  lastUpdate: 0,
+  data: null,
+};
+
+const CACHE_TIME = 10_000;
+
+// ===HELPERS===
+
+async function fetchFromSteam(url) {
+    const res = await fetch(url);
+    if(!res.ok)
+        throw new Error("Steam API error");
+    return res.json();
+}
+
+// ===OBTENER DATOS===
+
+async function obtenJuegoActual() {
+    const url = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${STEAM_KEY}&steamids=${STEAM_ID}`;
+    const data = await fetchFromSteam(url);
+    const player = data.response.players[0];
+    //console.log(player);
+    if(!player.gameid) return null;
+
+    return{
+        appid: player.gameid,
+        name: player.gameextrainfo,
+    };
+}
+
+//obtenJuegoActual();
+
+async function obtenerTiempoDeJuego(appid) {
+    const url = `http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${STEAM_KEY}&steamid=${STEAM_ID}&format=json`;
+    const data = await fetchFromSteam(url);
+    //console.log(data.response.games);
+
+    const juego = data.response.games?.find((g) => g.appid == appid);
+
+    if(!juego) return "0 hrs";
+
+    //console.log(`${(juego.playtime_forever/60).toFixed(1)} hrs`);
+
+    return `${(juego.playtime_forever/60).toFixed(1)} hrs`;
+
+}
+
+//obtenerTiempoDeJuego(2357570);
+
+async function obtenLogros(appid){
+    const url = `http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=${appid}&key=${STEAM_KEY}&steamid=${STEAM_ID}&l=latam`;
+    const data = await fetchFromSteam(url);
+    //console.log(data.playerstats.achievements);
+
+    return data.playerstats.achievements || [];
+}
+
+//obtenLogros(2357570);
+
+async function obtenerEsquema(appid) {
+    const url = `https://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key=${STEAM_KEY}&appid=${appid}&l=latam`;
+    const data = await fetchFromSteam(url);
+    return data.game.availableGameStats?.achievements || [];
+}
+
+//obtenerEsquema(2357570);
+
+function obtenUltimoLogro(userAch, schemaAch){
+
+    userAch = Array.isArray(userAch) ? userAch : [];
+    schemaAch = Array.isArray(schemaAch) ? schemaAch : [];
+
+    const desbloqueados = userAch
+    .filter((a) => a.achieved == 1)
+    .sort((a,b) => b.unlocktime - a.unlocktime);
+
+    //console.log(desbloqueado);
+
+    if(!desbloqueados.length) return [];
+
+    let ultimos3 = desbloqueados.slice(0,5);
+
+    return ultimos3.map(logro =>{
+        const meta = schemaAch.find(
+            s => s.displayName === logro.name
+        );
+
+        return{
+            name: meta?.displayName || logro.name,
+            image: meta?.icon || "",
+            unlocktime: logro.unlocktime,
+        };
+    });
+
+    
+}
+
+// ===ENDPOINT PRINCIPAL===
+
+app.get("/api/steam/achievements", async (req, res) => {
+    try{
+
+        STEAM_ID = req.query.steamid;
+        STEAM_KEY = req.query.steamkey;
+
+        const now = Date.now();
+
+        if(cache.data && now - cache.lastUpdate < CACHE_TIME){
+            return res.json(cache.data);
+        }
+
+        const juego = await obtenJuegoActual();
+
+        if(!juego){
+            const idle ={
+                active: false,
+                message: "Listo para monitorear",
+            };
+
+            cache = {lastUpdate: now, data: idle}
+            return res.json(idle);
+        }
+
+        const playtime = await obtenerTiempoDeJuego(juego.appid);
+        const logros = await obtenLogros(juego.appid);
+        const esquema = await obtenerEsquema(juego.appid);
+        const desbloqueado = logros.filter((a) => a.achieved == 1).length;
+        const total = logros.length;
+        const ultimoLogro = obtenUltimoLogro(logros, esquema);
+
+        const payload = {
+            active: true,
+            game:{
+                name: juego.name,
+                image: `https://cdn.cloudflare.steamstatic.com/steam/apps/${juego.appid}/header.jpg`,
+                timePlayed: playtime,
+            },
+            progress:{
+                desbloqueado,
+                total,
+                percentage: total ? Math.round((desbloqueado/total) * 100) : 0,
+            },
+            lastAchievements: ultimoLogro || {
+                name: "Sin logros recientes",
+                image: "",
+            },
+        };
+
+        console.log(payload);
+
+        cache = {lastUpdate: now, data: payload};
+        res.json(payload);
+    }catch(error){
+        console.error("Steam Error:", error);
+        res.status(500).json({
+            active: false,
+            message: "Error consultando Steam",
+        });
+    }
+})
+
+app.get('api/xbox/callback', async (req, res) => {
+    console.debug('Howdy xbox');
+})
+
+app.listen(process.env.PORT, () => {
+  console.log("Steam Widget Backend running on port", process.env.PORT);
+});
